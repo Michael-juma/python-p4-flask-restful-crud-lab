9539a27770f19bff38dde2d902c6828ed40eb62e
Juma Michael <obudho76@gmail.com>
2025-07-01 21:16:34 +0300

    all files

--- server/models.py
+++ server/models.py
@@ -1,16 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
+# Initialize SQLAlchemy
 db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
+    # Enable serialization of all fields by default
+    serialize_rules = ('-created_at', '-updated_at')  # optional
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    is_in_stock = db.Column(db.Boolean, nullable=False, default=True)
 
     def __repr__(self):
         return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+
